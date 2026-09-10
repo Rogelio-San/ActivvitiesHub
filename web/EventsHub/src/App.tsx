082e6e4252bd55d51ce234b6ca346fd292718330
Rogelio-San <rogelio.sandolo@gmail.com>
2026-09-10 08:57:27 -0600

    feat(parcial02) - add mui elements

--- web/EventsHub/src/App.tsx
+++ web/EventsHub/src/App.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react"
+import { Fragment, useEffect, useState } from "react"
+import { List, ListItem, ListItemText, Typography } from "@mui/material";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -12,14 +13,18 @@ function App() {
   }, []);
 
   return (
-    <div> 
-      <h3 style={{color : "coral"}}>Events Hub</h3>
-      <ul>
+    <Fragment> 
+      <Typography variant="h3" style={{ color: "coral" }}>
+        Events Hub
+      </Typography>
+      <List>
         {activities.map((activity: Activity) => (
-          <li key={activity.id}>{activity.title}</li>
+          <ListItem key={activity.id}>
+            <ListItemText>{activity.title}</ListItemText> 
+          </ListItem>
         ))}
-      </ul>
-    </div>
+      </List>
+    </Fragment>
   )
 }
 
